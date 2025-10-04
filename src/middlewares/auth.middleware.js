4b6ee8e6ba7cfb8869e3399aee95d7b73009b41e
sunny-rp <sunnyprajapar65351@gmail.com>
2025-10-04 17:30:13 +0530

    refactor: update parameter naming in verifyJWT middleware for clarity

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken"
 
 
 
-export const verifyJWT = asyncHandler(async (req,res,next) =>{
+export const verifyJWT = asyncHandler(async (req,_,next) =>{
       try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
